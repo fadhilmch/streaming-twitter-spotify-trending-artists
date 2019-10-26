c2dfab547320efce4108f9f4600d8374bf93e6cc
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-10-27 00:41:43 +0200

    modify cassandraConnedt

--- cassandraConnect.py
+++ cassandraConnect.py
@@ -7,10 +7,10 @@ class CassandraConnect:
         self.session = cluster.connect(keyspace ,wait_for_all_pools=True)
         self.session.execute('USE '+keyspace)
 
-    def get_data(self, time_range):
+    def get_data(self, time_range, table):
         now = datetime.datetime.now().replace(microsecond=0).isoformat()
         last_hour = (datetime.datetime.now() - datetime.timedelta(minutes=time_range)).replace(microsecond=0).isoformat()
-        rows = self.session.execute("SELECT artist, SUM(count) as total_count FROM TwitterTest WHERE date >= '"+last_hour+"' AND date <= '"+now+"' GROUP BY artist ALLOW FILTERING")
+        rows = self.session.execute("SELECT artist, SUM(count) as total_count FROM "+table+" WHERE date >= '"+last_hour+"' AND date <= '"+now+"' GROUP BY artist ALLOW FILTERING")
         return rows
 
 
