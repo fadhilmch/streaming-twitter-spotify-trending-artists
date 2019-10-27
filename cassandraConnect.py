44bae6ff08f1637940ddc2474bc840ba712e39cc
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-10-27 13:30:52 +0100

    changes on cassandraconnect to get data from cassandra

--- cassandraConnect.py
+++ cassandraConnect.py
@@ -7,10 +7,10 @@ class CassandraConnect:
         self.session = cluster.connect(keyspace ,wait_for_all_pools=True)
         self.session.execute('USE '+keyspace)
 
-    def get_data(self, time_range, table):
-        now = datetime.datetime.now().replace(microsecond=0).isoformat()
-        last_hour = (datetime.datetime.now() - datetime.timedelta(minutes=time_range)).replace(microsecond=0).isoformat()
-        rows = self.session.execute("SELECT artist, SUM(count) as total_count FROM "+table+" WHERE date >= '"+last_hour+"' AND date <= '"+now+"' GROUP BY artist ALLOW FILTERING")
+    def get_data(self, time_range, table, update_interval):
+        now = (datetime.datetime.now() - datetime.timedelta(seconds=update_interval)).replace(microsecond=0).isoformat()
+        last_hour = ((datetime.datetime.now() - datetime.timedelta(seconds=update_interval)) - datetime.timedelta(minutes=time_range)).replace(microsecond=0).isoformat()
+        rows = self.session.execute("SELECT artist, SUM(count) as total_count FROM "+table+" WHERE created_at >= '"+last_hour+"' AND created_at <= '"+now+"' GROUP BY artist ALLOW FILTERING")
         return rows
 
 
